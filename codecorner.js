1b1e4391602759df962ab3b7642353e0ea9c8517
Aiden Yang <alyang250@gmail.com>
2016-02-20 08:59:22 -0800

    backend connection working and listing out dummy posts onto welcome page

--- codecorner.js
+++ codecorner.js
@@ -2,6 +2,7 @@ Posts = new Mongo.Collection("posts");
 
 if (Meteor.isClient) {
 
+  Meteor.subscribe("posts");
 
   Router.configure({
   layoutTemplate: 'codecorner'
@@ -14,33 +15,34 @@ if (Meteor.isClient) {
   });
 
   Router.route('/post', function () {
-    this.render('post');
+    this.render('postList');
   });
+
+
 
  
   Session.setDefault('counter', 0);
 
-  Template.codecorner.helpers({
+
+  Template.welcome.helpers({
+    posts: function() {
+      return Posts.find();
+    },
+    length: function() {
+      return Posts.find().count();
+    }
+  });
+
+  Template.postList.helpers({
     counter: function () {
       return Session.get('counter');
+    },
+    posts: function () {
+      return Posts.find();
     }
   });
 
-  Template.codecorner.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
-
-
-  Template.post.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
-
-   Template.post.events({
+   Template.postList.events({
     'submit .new-post': function(event, template) {
         event.preventDefault();
         var title = event.target.title.value;
@@ -50,20 +52,27 @@ if (Meteor.isClient) {
           text: text,
           createdAt: new Date()
         });
-        console.log("we out here");
     }
   });
 
-   Template.post.rendered = function () {
+   Template.postList.rendered = function () {
     $('.summernote').summernote({
       height: 200
     });
   };
+
 }
+
+
+
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
 
+  });
+
+  Meteor.publish("posts", function () {
+    return Posts.find({});
   });
 }
 
