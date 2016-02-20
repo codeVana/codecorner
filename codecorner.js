7ead0a86a75e2aac20bd90b39052b23405f21216
Aiden Yang <alyang250@gmail.com>
2016-02-19 16:55:47 -0800

    aiden's first commit

--- codecorner.js
+++ codecorner.js
@@ -1,5 +1,22 @@
 if (Meteor.isClient) {
   // counter starts at 0
+
+
+  Router.configure({
+  layoutTemplate: 'codecorner'
+  });
+
+  Router.route('/', function () {
+    this.render('welcome', {
+      to:"main"
+    });
+  });
+
+  Router.route('/post', function () {
+    this.render('post');
+  });
+
+ 
   Session.setDefault('counter', 0);
 
   Template.codecorner.helpers({
@@ -14,10 +31,33 @@ if (Meteor.isClient) {
       Session.set('counter', Session.get('counter') + 1);
     }
   });
+
+
+  Template.post.helpers({
+    counter: function () {
+      return Session.get('counter');
+    }
+  });
+
+   Template.post.events({
+    'click button': function () {
+      // increment the counter when button is clicked
+      Session.set('counter', Session.get('counter') + 1);
+    }
+  });
+
+   Template.post.rendered = function () {
+    $('.summernote').summernote({
+      height: 100
+    });
+  };
 }
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
-    // code to run on server at startup
+
   });
 }
+
+
+
